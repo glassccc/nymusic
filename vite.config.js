a4960738d5ec4648b99857f51258445e73f169c5
glassccc <idosw825@gmail.com>
2025-05-03 23:13:31 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// root 경로를 my-webapp 폴더로 설정하지 않음 (상위 경로로부터 빌드)
 export default defineConfig({
+  base: '', // GitHub Pages 아니라서 base 필요 없음
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: '/manifest.json',  // 설정한 manifest.json 파일을 가리킴
-    }),
-  ],
-});
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
+})
